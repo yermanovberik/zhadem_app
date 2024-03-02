5a85678650d23612e0d72eafe0170bd081aeb7cb
yermanovberik <119285164+yermanovberik@users.noreply.github.com>
2024-03-02 15:31:40 +0500

    delete cors

--- src/main/java/com/app/zhardem/configurations/WebConfig.java
+++ src/main/java/com/app/zhardem/configurations/WebConfig.java
@@ -1,4 +1,4 @@
-package com.app.zhardem.configurations;
+/*package com.app.zhardem.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,3 +15,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true);
     }
 }
+*/
